3ee263ee0feb4a7ac5a1d63192d394169ad55082
Sergey Zabolotnyy <sergey.zabolotny@dataart.com>
2021-10-20 17:56:26 +0300

    main terminal app

--- store/main.swift
+++ store/main.swift
@@ -2,10 +2,66 @@
 //  main.swift
 //  store
 //
-//  Created by Zabolotnyy Sergey on 19.10.2021.
-//
 
 import Foundation
 
 print("Hello, World!")
+print ("SET <key> <value> store the value for key")
+print ("GET <key>         return the current value for key")
+print ("DELETE <key>      remove the entry for key")
+print ("COUNT <value>     return the number of keys that have the given value")
+print ("BEGIN             start a new transaction")
+print ("COMMIT            complete the current transaction")
+print ("ROLLBACK          revert to state prior to BEGIN call")
 
+let storage = TransactionalStorage()
+
+while let command = readLine(), command != "EXIT" {
+
+    let parsed = command.components(separatedBy: .whitespaces)
+
+    do {
+        switch parsed.first {
+        case "SET":
+            if let key: String = parsed[safe: 1], let value: String = parsed[safe: 2] {
+                storage.set(key: key, value: value)
+            } else {
+                print ("SET <key> <value> store the value for key")
+            }
+        case "GET":
+            if let key: String = parsed[safe: 1] {
+                print(try storage.get(key: key))
+            } else {
+                print ("GET <key> return the current value for key")
+            }
+
+        case "DELETE":
+            if let key: String = parsed[safe: 1] {
+                try storage.delete(key: key)
+            } else {
+                print ("DELETE <key> remove the entry for key")
+            }
+        case "COUNT":
+            if let value: String = parsed[safe: 1] {
+                print(storage.count(value: value))
+            } else {
+                print ("COUNT <value> return the number of keys that have the given value")
+            }
+        case "BEGIN":
+            storage.begin()
+        case "COMMIT":
+            print(try storage.commit())
+        case "ROLLBACK":
+            print(try storage.rollback())
+        default :
+            print("not supported")
+        }
+    } catch TransactionalStorage.TransactionalStorageError.noTransaction {
+        print("no transaction")
+    } catch TransactionalStorage.TransactionalStorageError.keyNotSet {
+        print("key not set")
+    } catch {
+        print("Unexpected error: \(error).")
+    }
+
+}

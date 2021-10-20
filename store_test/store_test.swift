690b0369f170894758b0b108ffe7ac22d3556f69
Sergey Zabolotnyy <sergey.zabolotny@dataart.com>
2021-10-20 17:55:51 +0300

    storage tests

---
+++ store_test/store_test.swift
@@ -0,0 +1,128 @@
+//
+//  store_test.swift
+//  store_test
+//
+//  Created by Zabolotnyy Sergey on 20.10.2021.
+//
+
+import XCTest
+@testable import store
+
+class store_test: XCTestCase {
+
+    var storage: TransactionalStorage?
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        storage = TransactionalStorage()
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        storage = nil
+    }
+
+    func testSetGet() throws {
+        storage?.set(key: "foo", value: "123")
+        XCTAssertEqual(try storage?.get(key: "foo"), "123", "Not valid value")
+
+        storage?.set(key: "foo", value: "456")
+        XCTAssertEqual(try storage?.get(key: "foo"), "456", "Not valid value")
+    }
+
+    func testDelete() throws {
+        storage?.set(key: "foo", value: "123")
+        do {
+            try storage?.delete(key: "foo")
+        } catch {
+            XCTFail("wrong error")
+        }
+        do {
+            try storage?.get(key: "foo")
+        } catch TransactionalStorage.TransactionalStorageError.keyNotSet{
+            print("keyNotSet")
+        } catch {
+            XCTFail("wrong error")
+        }
+    }
+
+    func testCount() {
+        storage?.set(key: "foo", value: "123")
+        storage?.set(key: "bar", value: "456")
+        storage?.set(key: "baz", value: "123")
+        XCTAssertEqual(storage?.count(value: "123"), 2, "Not valid value")
+        XCTAssertEqual(storage?.count(value: "456"), 1, "Not valid value")
+    }
+
+    func testCommitTransaction() {
+        storage?.begin()
+        storage?.set(key: "foo", value: "456")
+        do {
+            try storage?.commit()
+        } catch {
+            XCTFail("wrong error")
+        }
+        do {
+            try storage?.rollback()
+        } catch TransactionalStorage.TransactionalStorageError.noTransaction {
+            print("noTransaction")
+        } catch {
+            XCTFail("wrong error")
+        }
+
+        XCTAssertEqual(try storage?.get(key: "foo"), "456", "Not valid value")
+    }
+
+    func testRollbackTransaction() {
+        storage?.set(key: "foo", value: "123")
+        storage?.set(key: "bar", value: "abc")
+        storage?.begin()
+        storage?.set(key: "foo", value: "456")
+
+        XCTAssertEqual(try storage?.get(key: "foo"), "456", "Not valid value")
+
+        storage?.set(key: "bar", value: "def")
+        XCTAssertEqual(try storage?.get(key: "bar"), "def", "Not valid value")
+        do {
+            try storage?.rollback()
+        } catch {
+            XCTFail("wrong error")
+        }
+
+        XCTAssertEqual(try storage?.get(key: "foo"), "123", "Not valid value")
+        XCTAssertEqual(try storage?.get(key: "bar"), "abc", "Not valid value")
+        do {
+            try storage?.commit()
+        } catch TransactionalStorage.TransactionalStorageError.noTransaction{
+            print("noTransaction")
+        } catch {
+            XCTFail("wrong error")
+        }
+    }
+
+    func testNestedTransactions() {
+        storage?.set(key: "foo", value: "123")
+        storage?.begin()
+        storage?.set(key: "foo", value: "456")
+        storage?.begin()
+        storage?.set(key: "foo", value: "789")
+
+        XCTAssertEqual(try storage?.get(key: "foo"), "789", "Not valid value")
+
+        do {
+            try storage?.rollback()
+        } catch {
+            XCTFail("wrong error")
+        }
+
+        XCTAssertEqual(try storage?.get(key: "foo"), "456", "Not valid value")
+
+        do {
+            try storage?.rollback()
+        } catch {
+            XCTFail("wrong error")
+        }
+
+        XCTAssertEqual(try storage?.get(key: "foo"), "123", "Not valid value")
+    }
+}

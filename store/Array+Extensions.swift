71ed77c11e971014a6c75f6847b4ef9ecdfacde6
Sergey Zabolotnyy <sergey.zabolotny@dataart.com>
2021-10-20 17:55:03 +0300

    array sugar

---
+++ store/Array+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Array+Extensions.swift
+//  store
+//
+
+import Foundation
+
+extension Array {
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
